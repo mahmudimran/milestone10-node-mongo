b6f0f1a7b282e9f10c24631574d6b6776509e319
mahmudimran <miahimran75@gmail.com>
2021-05-17 23:03:10 +0600

    new codde add

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const app = express();
 
 
 app.get('/', (req,res) =>{
-    res.send(' Hello Node js')
+    const product ={
+        name: 'LED TV',
+        price : 200,
+    }
+    res.send(product)
 
 })
 
+app.get('/product/banana', (req,res) =>{
+    res.send({furit: 'bannaa', quantity: 3000, price: 2000})
+})
 
 app.listen(3800,() => console.log('listent this port 3800'))
